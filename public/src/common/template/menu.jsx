c6ffb8a7d355ba26ab6ba46538aa6dc306d0902f
AlbertoParente <albertoparente20@gmail.com>
2021-07-09 22:54:42 -0300

    Creating Menu component, continuation

--- public/src/common/template/menu.jsx
+++ public/src/common/template/menu.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
 
 export default props => (
     <ul className='sidebar-menu'>
-        
+        <MenuItem path="#/" label='Dashboard' icon="dashboard" />
+        <MenuTree label="Register" icon="edit">
+            <MenuItem path="#billingCycles" label="Cycles to the Payment" icon="usd" />
+        </MenuTree>
+
+
     </ul>
 )
