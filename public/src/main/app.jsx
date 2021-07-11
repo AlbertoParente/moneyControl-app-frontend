28ac0e7704ea756478a4068efbd91a7ad349afe9
AlbertoParente <albertoparente20@gmail.com>
2021-07-10 22:25:29 -0300

    Creating footer, dasboard and billingCycle component

--- public/src/main/app.jsx
+++ public/src/main/app.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import '../common/template/dependencies'
 import Header from '../common/template/header'
 import SideBar from '../common/template/SideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Content</h1>
+        </div>
+        <Footer />
     </div>
 )
