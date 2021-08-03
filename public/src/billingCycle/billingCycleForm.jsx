6caa46df0ac8e529751db5884655e9cabc9a66ac
AlbertoParente <albertoparente20@gmail.com>
2021-08-02 23:33:28 -0300

    Adding fild with custom components

--- public/src/billingCycle/billingCycleForm.jsx
+++ public/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, { component } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
 
@@ -9,9 +10,12 @@ class BillingCycleForm extends Component {
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className={box-body}>
-                    <Field name='name' component='input' />
-                    <Field name='month' component='input' />
-                    <Field name='year' component='input' />
+                    <Field name='name' component={labelAndInput}
+                        label="Name" cols="12 4" placeholder="Enter The Name" />
+                    <Field name='month' component={labelAndInput} type="number"
+                        label="Month" cols="12 4" placeholder="Enter The Month" />
+                    <Field name='year' component={labelAndInput} type="number"
+                        label="Age" cols="12 4" placeholder="Enter The Age" />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className="btn btn-primary">Submit</button>
