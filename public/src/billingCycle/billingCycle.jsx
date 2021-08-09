afd387c572922a078dbe9d63279532ff9bceb1e5
AlbertoParente <albertoparente20@gmail.com>
2021-08-08 23:39:26 -0300

    Removing test and finishing basic registration

--- public/src/billingCycle/billingCycle.jsx
+++ public/src/billingCycle/billingCycle.jsx
@@ -37,45 +37,18 @@ class BillingCycle extends Component {
                                 <List />
                             </TabContent>
                             <TabContent id='tabCreate'>
-                                <Form onSubmit={this.props.create} />
+                                <Form onSubmit={this.props.create} submitLabel='Include' submitClass="primary" />
                             </TabContent>
                             <TabContent id='tabUpdate'>
-                                <Form onSubmit={this.props.update} />
+                                <Form onSubmit={this.props.update} submitLabel='Alter' submitClass="info" />
                             </TabContent>
                             <TabContent id='tabDelete'>
-                                <Form onSubmit={this.props.remove} readOnly={true} />
+                                <Form onSubmit={this.props.remove} readOnly={true} submitLabel='Delete' submitClass="danger" />
                             </TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
             </div>
-            <div>
-            <ContentHeader title="Payment Cycles" small="Register" />
-            <Content>
-                <Tabs>
-                    <TabsHeader>
-                        <TabHeader label="List" icon="bars" target="tabList" />
-                        <TabHeader label="Add" icon="plus" target="tabCreate" />
-                        <TabHeader label="Alter" icon="pencil" target="tabUpdate" />
-                        <TabHeader label="Delete" icon="trash-o" target="tabDelete" />
-                    </TabsHeader>
-                    <TabsContent>
-                        <TabContent id="tabList">
-                            <List />
-                        </TabContent>
-                        <TabContent id='tabCreate'>
-                            <Form onSubmit={this.props.create} />
-                        </TabContent>
-                        <TabContent id='tabUpdate'>
-                            <Form onSubmit={this.props.update} />
-                        </TabContent>
-                        <TabContent id='tabDelete'>
-                            <Form onSubmit={this.props.remove} readOnly={true} />
-                        </TabContent>
-                    </TabsContent>
-                </Tabs>
-            </Content>
-        </div>
         )
     }
 }
--- public/src/billingCycle/billingCycleList.jsx
+++ public/src/billingCycle/billingCycleList.jsx
@@ -24,19 +24,6 @@ class BillingCycleList extends Component {
                     </button>
                 </td>
             </tr>
-            <tr key={bc._id}>
-            <td>{bc.name}</td>
-            <td>{bc.month}</td>
-            <td>{bc.year}</td>
-            <td>
-                <button className="btn btn-warning" onClick={() => this.props.showUpdate(bc)}>
-                    <i className="fa fa-pencil"></i>
-                </button>
-                <button className="btn btn-danger" onClick={() => this.props.showDelete(bc)}>
-                    <i className="fa fa-trash-o"></i>
-                </button>
-            </td>
-        </tr>
         ))
     }
 
@@ -48,7 +35,7 @@ class BillingCycleList extends Component {
                         <tr>
                             <th>Name</th>
                             <th>Month</th>
-                            <th>Year</th>
+                            <th className="table-actions" >Year</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -56,20 +43,6 @@ class BillingCycleList extends Component {
                     </tbody>
                 </table>
             </div>
-            <div>
-            <table className='table'>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Month</th>
-                        <th>Year</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.renderRows()}
-                </tbody>
-            </table>
-        </div>
         )
     }
 }
