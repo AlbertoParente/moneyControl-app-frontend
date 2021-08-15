0171c9c02762dcd984d8712526474b1ddd78836b
AlbertoParente <albertoparente20@gmail.com>
2021-08-15 14:15:50 -0300

    Adding status field

--- public/src/billingCycle/billingCycleForm.jsx
+++ public/src/billingCycle/billingCycleForm.jsx
@@ -9,11 +9,11 @@ import ItemList from './itemList'
 class BillingCycleForm extends Component {
 
     render() {
-        const { handleSubmit, readOnly, credits } = this.props
+        const { handleSubmit, readOnly, credits, debts } = this.props
 
         return (
             <form role="form" onSubmit={handleSubmit}>
-                <div className={box-body}>
+                <div className={box - body}>
                     <Field name='name' component={LabelAndInput} readOnly={readOnly}
                         label="Name" cols="12 4" placeholder="Enter The Name" />
                     <Field name='month' component={LabelAndInput} type="number" readOnly={readOnly}
@@ -21,7 +21,9 @@ class BillingCycleForm extends Component {
                     <Field name='year' component={LabelAndInput} type="number" readOnly={readOnly}
                         label="Age" cols="12 4" placeholder="Enter The Age" />
                     <ItemList cols="12 6" list={credits} readOnly={readOnly}
-                        field="credits" legend="Credits"/>
+                        field="credits" legend="Credits" />
+                    <ItemList cols="12 6" list={debts} readOnly={readOnly}
+                        field="debts" legend="Debts" showStatus={true} />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
@@ -34,6 +36,6 @@ class BillingCycleForm extends Component {
 
 BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
 const selector = formValueSelector('billingCycleForm')
-const mapStateToProps = state => ({credits: selector(state, 'credits')})
+const mapStateToProps = state => ({ credits: selector(state, 'credits'), debts: selector(state, 'debts') })
 const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
--- public/src/billingCycle/itemList.jsx
+++ public/src/billingCycle/itemList.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Fild, arrayInsert, arrayRemove } from 'react-form'
-import { Field } from 'redux-form'
+import { Field, arrayInsert, arrayRemove } from 'react-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
+import If from '../common/operator/if'
+
 
 class ItemList extends Component {
 
     add(index, item = {}) {
-        if(!this.props.readOnly) {
+        if (!this.props.readOnly) {
             this.props.arrayInsert('billingCycleForm', this.props.field, index, item)
         }
     }
 
     remove(index) {
-        if(!this.props.readOnly && this.props.list.length > 1) {
+        if (!this.props.readOnly && this.props.list.length > 1) {
             this.props.arrayRemove('billingCycleForm', this.props.field, index)
         }
     }
@@ -24,10 +25,14 @@ class ItemList extends Component {
         const list = this.props.list || []
         return list.map((item, index) => {
             <tr key={index}>
-                <td><Field name={`${this.props.field}[${index}].name`} component={input}
+                <td><Field name={`${this.props.field}[${index}].name`} component={Input}
                     placeholder="Enter the name" readOnly={this.props.readOnly}></Field></td>
-                <td><Field name={`${this.props.field}[${index}].value`} component={input}
+                <td><Field name={`${this.props.field}[${index}].value`} component={Input}
                     placeholder="Enter the value" readOnly={this.props.readOnly}></Field></td>
+                <If test={this.props.showStatus}>
+                    <td><Field name={`${this.props.field}[${index}].status`} component={Input}
+                        placeholder="Enter the status" readOnly={this.props.readOnly}></Field></td>
+                </If>
                 <td>
                     <button type="button" className="btn btn-success"
                         onClick={() => this.add(index + 1)}>
@@ -57,6 +62,9 @@ class ItemList extends Component {
                             <tr>
                                 <th>Name</th>
                                 <th>Value</th>
+                                <If test={this.props.showStatus}>
+                                    <th>Status</th>
+                                </If>
                                 <th className="table-actions">Actions</th>
                             </tr>
                         </thead>
