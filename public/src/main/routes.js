3fcd1614bd0960115b343eb3377525e25366b6e7
AlbertoParente <albertoparente20@gmail.com>
2021-08-18 23:27:14 -0300

    Updating navigation with react-router

--- public/src/main/routes.js
+++ public/src/main/routes.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Router, Router, Redirect, hashHistory } from 'react-router'
+import { Router, Router, IndexRoute, Redirect, hashHistory } from 'react-router'
+import App from './app'
 import Dashboard from '../dashboard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => {
     <Router history={hashHistory}>
-        <Router path="/" component={Dashboard}/>
-        <Router path="/billingCycles" component={BillingCycle}/>
+        <Route path='/' component={App}>
+            <IndexRoute component={Dashboard} />
+            <Router path="billingCycles" component={BillingCycle} />
+        </Route>
         <Redirect from="*" to="/" />
     </Router>
 }
